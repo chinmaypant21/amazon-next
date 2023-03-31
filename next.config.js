4ac3d210b7f0645006a2d2fbe01504d35fee4178
chinmaypant21 <pant.chinu.2@gmail.com>
2023-03-31 10:35:35 +0530

    [IMP] -> next-ts dev error config

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// @ts-check
+module.exports = {
+    webpack(config, { dev }) {
+      if (dev) {
+        const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+        config.plugins.push(new ForkTsCheckerWebpackPlugin());
+      }
+      return config;
+    },
+  };
