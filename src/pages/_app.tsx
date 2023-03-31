3630987d10b4cc8f68f509b11a2b939491007d3a
chinmaypant21 <pant.chinu.2@gmail.com>
2023-03-31 10:34:35 +0530

    Typescript initialization

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import { Provider } from 'react-redux'
 import { store } from '../app/store'
 import '../styles/globals.css'
-
 const MyApp = ({ Component, pageProps }) => {
   return (
     <Provider store={store}>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Header from "../components/Header/Header";
 
 export default function Home() {
   return (
@@ -6,6 +7,7 @@ export default function Home() {
       <Head>
         <title>Amazon</title>
       </Head>
+      <Header></Header>
     </div>
   );
 }
