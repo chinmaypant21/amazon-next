d1160d62f0df3f2b99b93d4c0e15bd5465debdd3
chinmaypant21 <pant.chinu.2@gmail.com>
2023-04-12 19:17:10 +0530

    Cart Feed

--- src/components/Cart/CartFeed.tsx
+++ src/components/Cart/CartFeed.tsx
@@ -1,23 +1,47 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
-import { itemCount } from '../../state/slices/cartSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import { itemCount, removeFromCart, selectCartItems } from '../../state/slices/cartSlice'
+import { cartItemType } from '../../utils/commonTypes'
 
 const CartFeed = ({authStatus}) => {
+  const cartItems : cartItemType[] = useSelector(selectCartItems)
+  const dispatch = useDispatch()
   const itemsCount : number = useSelector(itemCount)
+
+  const handleItemRemove = (id) => {
+    dispatch(removeFromCart(id))
+  }
 
   if (authStatus === 'authenticated')
     return (
       <>
       {
         (itemsCount)
-        ? <div>Shopping Cart</div>
-        : <div>Your Amazon Cart is empty</div>
-      }
+        ? <div>
+            <div>Shopping Cart</div>
+            
+            <div>
+            {
+              cartItems.map( item => (
+                <div key={item.id}>
+                  {/* {item.image} */}
+                  {item.description}
+                  {/* {item.price} */}
+                  {item.quantity}
+                  <div onClick={() => {handleItemRemove(item.id)}}>Remove</div>
+                </div>
+              ))
+            }
+            </div>
+          </div>
+
+: <div>Your Amazon Cart is empty</div>
+}
       </>
     )
-
-  else return (
-    <>
+    
+    else return (
+      <>
       Unauthenticated
     </>
   )
