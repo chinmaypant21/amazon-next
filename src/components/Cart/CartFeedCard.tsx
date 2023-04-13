ce63178a6cda0b1f60e561bba9074d4c026b079b
chinmaypant21 <pant.chinu.2@gmail.com>
2023-04-13 17:20:44 +0530

    Cart Responsiveness

--- src/components/Cart/CartFeedCard.tsx
+++ src/components/Cart/CartFeedCard.tsx
@@ -21,40 +21,43 @@ const CartFeedCard = ({item} : {item: cartItemType}) => {
   return (
     <>
         <hr className='' />
-        <div className='flex gap-x-6 justify-between' key={item.id}>
-        <Image className='object-contain' loading='lazy' src={item.image} width={100} height={100} alt={item.category} />
-        <div className='flex flex-col gap-y-1  justify-start w-full'>
-            <span className='font-semibold'>{item.title}</span>
-            <span className='text-sm line-clamp-2'>{item.description}</span>
-            <Rating name="read-only" 
-            value={item.rating.rate} 
-            precision={0.5}
-            size='small'
-            sx={{color:'#fea21c'}}
-            readOnly 
-            />
-            <span>₹<CurrencyFormat className='text-md font-medium leading-none'
-                value={item.price * 82} 
-                displayType='text'
-                thousandSeparator={true}
-                decimalScale={2}
-                fixedDecimalScale={true}
-                />
-            </span>
+        <div className='flex flex-col md-s:flex-row gap-x-4' key={item.id}>
+            <div className='flex flex-row flex-[8] gap-x-6 '>
+                <Image className='object-contain' loading='lazy' src={item.image} width={100} height={100} alt={item.category} />
+                    <div className='flex flex-col gap-y-1 overflow-hidden justify-start w-full'>
+                        <span className='font-semibold'>{item.title}</span>
+                        <span className='text-sm line-clamp-2'>{item.description}</span>
+                        <Rating name="read-only"
+                          value={item.rating.rate}
+                          precision={0.5}
+                          size='small'
+                          sx={{ color: '#fea21c' }}
+                          readOnly
+                        />
+                        <span>₹<CurrencyFormat className='text-md font-medium leading-none'
+                          value={item.price * 82}
+                          displayType='text'
+                          thousandSeparator={true}
+                          decimalScale={2}
+                          fixedDecimalScale={true}
+                        /></span>
 
-            <div className='my-2 flex justify-center'>
-                <span className='bg-gray-300 rounded-md p-1 font-semibold'>Qty : {item.quantity}</span>
-            </div>
-
+                        <div className='my-2 flex justify-start md-s:justify-center'>
+                          <span className='bg-gray-300 rounded-md p-1 font-semibold'>Qty : {item.quantity}</span>
+                        </div>
+                    </div>
         </div>
 
-        <div className='flex flex-col gap-y-3'>
-            <div className='amzn-btn-main cursor-pointer' onClick={() => {handleItemAdd(item)}}>
-                <span>Add</span>
-            </div>
-            
-            <div className='amzn-btn-main pt-10 cursor-pointer' onClick={() => {handleItemRemove(item.id)}}>
-                <span>Remove</span>
+        <div className='flex items-center flex-[1]'>
+            <div className='flex flex-row
+                    md-s:flex-col gap-x-3 md-s:gap-y-2 justify-center w-full'>
+                <div className='amzn-btn-main cursor-pointer' onClick={() => {handleItemAdd(item)}}>
+                    <span>Add</span>
+                </div>
+                
+                <div className='amzn-btn-main pt-10 cursor-pointer' onClick={() => {handleItemRemove(item.id)}}>
+                    <span>Remove</span>
+                </div>
             </div>
         </div>
         </div>
--- src/components/Cart/CartPayout.tsx
+++ src/components/Cart/CartPayout.tsx
@@ -5,17 +5,55 @@ import { useSelector } from 'react-redux';
 import { selectCartItems } from '../../state/slices/cartSlice';
 import CurrencyFormat from 'react-currency-format';
 
-const CartPayout : React.FC<{authStatus : string}> = ({authStatus}) : JSX.Element => {
-    const cartItems : cartItemType[] = useSelector(selectCartItems)
+const PayoutFeed : React.FC<{items: cartItemType[]}> = ({items}) : JSX.Element => {
 
     const getPayoutAmount = (items :cartItemType[]) : number => (
-        items.map(item => item.price * item.quantity * 82).reduce((x,y)=>x+y)
+        (items.length > 0)
+        ? items.map(item => item.price * item.quantity * 82).reduce((x,y)=>x+y)
+        : 0 
     )
 
     const getTotalCount = (items : cartItemType[]) : number => (
-        items.map(item => item.quantity).reduce((x,y)=>x+y)
+        (items.length > 0)
+        ? items.map(item => item.quantity).reduce((x,y)=>x+y)
+        : 0
+        
     )
 
+    if (items.length > 0) {
+        return (
+            <div className='flex flex-col gap-y-4'>
+            <span className='text-sm text-[#008500] font-semibold'>Your order qualifies for FREE Delivery.</span>
+            <div>
+                <div className='text-center '>
+                    <span>{`Subtotal (${getTotalCount(items)} items): `}</span>
+                    <span className='text-sm font-semibold'>₹</span>
+                    <CurrencyFormat 
+                    className='text-lg font-medium leading-none'
+                    value={getPayoutAmount(items)} 
+                    displayType='text'
+                    thousandSeparator={true}
+                    decimalScale={2}
+                    fixedDecimalScale={true}/>
+                </div>
+                <span><input type='checkbox'/> This order contains a gift</span>
+            </div>
+
+                <div className='flex flex-col px-2'>
+                    <span className='amzn-btn-main'>Proceed to buy</span>
+                </div>
+            </div>
+    )
+    }
+
+    else return (
+        <div className='text-lg weight-bold'>Add items to cart</div>
+    )
+}
+
+const CartPayout : React.FC<{authStatus : string}> = ({authStatus}) : JSX.Element => {
+    const cartItems : cartItemType[] = useSelector(selectCartItems)
+    
     const handleSignIn = () => {
         signIn()
     }
@@ -24,27 +62,7 @@ const CartPayout : React.FC<{authStatus : string}> = ({authStatus}) : JSX.Elemen
         <div className='px-2 flex justify-center my-10'>
             {
                 (authStatus === 'authenticated')
-                ? <div className='flex flex-col gap-y-4'>
-                    <span className='text-sm text-[#008500] font-semibold'>Your order qualifies for FREE Delivery.</span>
-                    <div>
-                        <div className='text-center '>
-                            <span>{`Subtotal (${getTotalCount(cartItems)} items): `}</span>
-                            <span className='text-sm font-semibold'>₹</span>
-                            <CurrencyFormat 
-                            className='text-lg font-medium leading-none'
-                            value={getPayoutAmount(cartItems)} 
-                            displayType='text'
-                            thousandSeparator={true}
-                            decimalScale={2}
-                            fixedDecimalScale={true}/>
-                        </div>
-                        <span><input type='checkbox'/> This order contains a gift</span>
-                    </div>
-
-                    <div className='flex flex-col px-2'>
-                        <span className='amzn-btn-main'>Proceed to buy</span>
-                    </div>
-                </div>
+                ? <PayoutFeed items={cartItems}/>
 
                 : <div className='flex flex-col gap-y-3'>
                     <span>You need to login to proceed</span>
